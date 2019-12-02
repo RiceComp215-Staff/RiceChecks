dfb55d8fb8afdce4e370dbff597f0d629ab5b156
Dan Wallach <dwallach@rice.edu>
2019-12-02 15:39:36 -0600

    trying to debug GitHub Actions

--- run-github-actions.sh
+++ run-github-actions.sh
@@ -12,3 +12,7 @@ cat exampleSort/build/autograder/report.txt
 echo ============= standaloneSort/
 cat standaloneSort/build/autograder/report.txt
 exit $retVal
+
+# debugging for GitHub Actions
+echo "Where are the google-java-format directories?"
+find . -name google-java-format -print | xargs ls
