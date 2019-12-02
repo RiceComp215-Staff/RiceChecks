a69ccd1c3da2160c0608270e9dae91f44ba694df
Dan Wallach <dwallach@rice.edu>
2019-12-02 15:34:56 -0600

    added shell script to try to make output make more sense

---
+++ run-github-actions.sh
@@ -0,0 +1,14 @@
+# this script avoids a weird problem where Gradle is printing the autograder output at the wrong time
+
+export RICECHECKS_QUIET=true
+./gradlew --console=plain :autograder:check autograder
+retVal=$?
+echo ============= exampleRegex/
+cat exampleRegex/build/autograder/report.txt
+echo ============= exampleRpn/
+cat exampleRpn/build/autograder/report.txt
+echo ============= exampleSort/
+cat exampleSort/build/autograder/report.txt
+echo ============= standaloneSort/
+cat standaloneSort/build/autograder/report.txt
+exit $retVal
