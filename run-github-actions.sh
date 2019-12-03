c9ec728e47e07514c31d843ada5a1521d5438400
Dan Wallach <dwallach@cs.rice.edu>
2019-12-02 20:11:07 -0600

    updated version number to 0.8.0, ready to build

--- run-github-actions.sh
+++ run-github-actions.sh
@@ -21,8 +21,8 @@ echo
 cat standaloneSort/build/autograder/report.txt
 
 # debugging for GitHub Actions
-echo 'Where are the google-java-format directories?'
-find . -name google-java-format -print | xargs ls -lR > tmp
-cat tmp
+# echo 'Where are the google-java-format directories?'
+# find . -name google-java-format -print | xargs ls -lR > tmp
+# cat tmp
 
 exit $retVal
